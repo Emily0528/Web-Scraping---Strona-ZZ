c9b8c9f537e852be681006cab49faf3f94403fda
Emily0528 <157897067+Emily0528@users.noreply.github.com>
2024-06-08 12:06:59 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,3 +1,9 @@
-function siema() {
-  console.log("test");
-}
+document.addEventListener("DOMContentLoaded", function () {
+  var textContainer = document.querySelector(".text-container");
+  // Szerokość kontenera tekstowego
+  var containerWidth = textContainer.clientWidth;
+  // Pozycja środka kontenera
+  var containerCenter = containerWidth / 2;
+  // Przewijanie do pozycji środka kontenera
+  textContainer.scrollLeft = containerCenter;
+});
